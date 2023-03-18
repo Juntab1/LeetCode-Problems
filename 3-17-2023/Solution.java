552c1aaf773e1693817d8714105c98056793b86e
juntab1 <jsbwhaleguy@gmail.com>
2023-03-17 17:39:03 -0700

    problem complete

--- 3-17-2023/Solution.java
+++ 3-17-2023/Solution.java
@@ -1,5 +1,33 @@
 import java.util.*;
 
+// This was practice using a hashMap and also using a boolean list to check 
+// if the two words are isomorphic or not.
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        boolean[] check = new boolean[128];
+        Map<Character, Character> answer = new HashMap<>();
+        for (int i = 0; i < s.length(); i++){
+            char sChar = s.charAt(i);
+            char tChar = t.charAt(i);  
+            if (!answer.containsKey(sChar)){
+                if (check[tChar]){
+                return false;
+                }
+                check[tChar] = true;
+                answer.put(sChar, tChar);
+            }
+            else{
+                if (!answer.get(sChar).equals(tChar)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
+
+// DID NOT HAVE THE BOOLEAN LIST CHECK TO SEE IF THE WORD 
+// IS ALREADY ASSOCIATED OR NOT.
 // This solution does not work fully. It can't pass a few tests
 // class Solution {
 //     public boolean isIsomorphic(String s, String t) {
