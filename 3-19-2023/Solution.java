5596be86d4b8d8cc9448d842b26b5b9d64f99b6f
juntab1 <jsbwhaleguy@gmail.com>
2023-03-19 19:11:29 -0700

    problem solved

--- 3-19-2023/Solution.java
+++ 3-19-2023/Solution.java
@@ -1,10 +1,27 @@
-package 3-19-2023;
-
 // This is a linked List problem that can be done iterativly or recursivly. I tried to do
 // recursive but it was harder than I thought. I need to practice a little more on my recursion.
 // However, I understood the iteration way.
 // The idea is that you need to keep a temp variable that stores everything else than the first 
 // variable in your curr and your PREV variable keeps track of your reverse order of nodes
-public class Solution {
-    
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode curr = head;
+        ListNode reverse = null;
+
+        while (curr != null){
+            ListNode temp = curr.next;
+            curr.next = reverse;
+            reverse = curr;
+            curr = temp;
+        }
+        return reverse;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
 }
