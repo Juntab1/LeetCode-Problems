993f311d8edbde032d4fe46669182ffe7cc6de02
juntab1 <jsbwhaleguy@gmail.com>
2023-03-19 17:27:08 -0700

    problem solved

--- 3-18-2023/Solution.java
+++ 3-18-2023/Solution.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -9,6 +11,33 @@
  * }
  */
 
+// This solution works. Next time draw it out more to figure it out before 
+// thinking you know how to do it and blindly writing code out.
+ class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode answer = new ListNode(-1);
+        ListNode curr = answer;
+        while (list1 != null && list2 != null){
+            if (list1.val <= list2.val){
+                curr.next = list1;
+                list1 = list1.next;
+            }
+            else{
+                curr.next = list2;
+                list2 = list2.next;
+            }
+            curr = curr.next;
+        }
+        if (list1 == null){
+            curr.next = list2;
+        }
+        else{
+            curr.next = list1;
+        }
+
+        return answer.next; 
+    }
+}
 
 // This approach did not work and I can't really figure out why.
 // However, looking at solutions there is a recursive way to do this
@@ -17,7 +46,6 @@
 //          if (list1 == null) return list2;
 //          if (list2 == null) return list1;
 //         ListNode answer = list1;
-//         // curr.next = new ListNode(list2.val,curr.next);
 //         if (answer.val > list2.val){
 //             answer = new ListNode(list2.val, answer);
 //             list2 = list2.next;
