45b4b0dae0f24fe6d9806b0e72711f794eb7a092
juntab1 <jsbwhaleguy@gmail.com>
2023-03-19 18:22:47 -0700

    problem 1 of day Idea

---
+++ 3-19-2023/Solution.java
@@ -0,0 +1,10 @@
+package 3-19-2023;
+
+// This is a linked List problem that can be done iterativly or recursivly. I tried to do
+// recursive but it was harder than I thought. I need to practice a little more on my recursion.
+// However, I understood the iteration way.
+// The idea is that you need to keep a temp variable that stores everything else than the first 
+// variable in your curr and your PREV variable keeps track of your reverse order of nodes
+public class Solution {
+    
+}
