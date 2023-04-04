149dbfd14ef261a034a2058d09a538d0985f2dc4
juntab1 <jsbwhaleguy@gmail.com>
2023-04-03 22:29:49 -0700

    problem 2 of day

---
+++ 4-3-232/Solution2.java
@@ -0,0 +1,20 @@
+// almost complete just have to work out a few bugs.
+class Solution2{
+    public int searchInsert(int[] nums, int target) {
+        int leftBound = 0;
+        int rightBound = nums.length-1;
+        while (rightBound > leftBound){
+            int mid = (rightBound-leftBound)/2;
+            if (nums[mid] == target){
+                return mid;
+            }
+            if (target >= nums[mid]){
+                leftBound = mid;
+            }
+            else if (target < nums[mid]){
+                rightBound = mid-1;
+            }
+        }
+        return leftBound;
+    }
+}
