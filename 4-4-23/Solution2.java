4dc39d51f2707ba03f1d22c21c833f1846307124
juntab1 <jsbwhaleguy@gmail.com>
2023-04-05 21:38:26 -0700

    2nd try better solution

--- 4-4-23/Solution2.java
+++ 4-4-23/Solution2.java
@@ -2,20 +2,27 @@
 // liner time complexity but there is a more effiecient way of solving this problem using XOR. I need to
 // look more into XOR tommorow. So far proud of myself for heading on the right track and getting 2 questions
 // correct in a row so far!
+
+// Doing XOR is so much easier! I think I mostly understand how XOR works.
 class Solution {
     public int singleNumber(int[] nums) {
-        Map<Integer,Integer> keep = new HashMap<>();
-        for (int n : nums){
-            if (!keep.containsKey(n)){
-                keep.put(n, 0);
-            }
-            keep.put(n, keep.get(n)+1);
+        int finalNum = 0;
+        for (int i = 0; i < nums.length; i++){
+            finalNum ^= nums[i];
         }
-        for (int key : keep.keySet()){
-            if (keep.get(key) != 2){
-                return key;
-            }
-        }
-        return 0;
+        return finalNum;
+        // Map<Integer,Integer> keep = new HashMap<>();
+        // for (int n : nums){
+        //     if (!keep.containsKey(n)){
+        //         keep.put(n, 0);
+        //     }
+        //     keep.put(n, keep.get(n)+1);
+        // }
+        // for (int key : keep.keySet()){
+        //     if (keep.get(key) != 2){
+        //         return key;
+        //     }
+        // }
+        // return 0;
     }
 }
