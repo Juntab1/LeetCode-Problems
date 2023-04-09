f9e00dab27914e480faa9b1dd2c499ebb08071d1
juntab1 <jsbwhaleguy@gmail.com>
2023-04-09 00:37:44 -0700

    problem solved

--- 4-8-2023/Solution.java
+++ 4-8-2023/Solution.java
@@ -1,17 +1,32 @@
-// having a problem with my for loop to find if the jump actually works
+// This was a hard problem for me but I understand it now after looking at the solutions.
 class Solution {
     public boolean canJump(int[] nums) {
         int maxLen = nums.length;
-        boolean[] ans = new boolean[maxLen];
-        for (int i = 0; i < maxLen; i++){
-            ans[i] = false;
-        }
-        ans[maxLen-1] = true;
+        int goal = maxLen - 1;
         for (int j = maxLen-2; 0 <= j; j--){
-            if ((j+1) <= (j+nums[j])){
-                ans[j] = true;
+            if (goal <= (j+nums[j])){
+                goal = j;
             }
         }
-        return ans[0];
+        return goal == 0;
     }
 }
+
+
+// having a problem with my for loop to find if the jump actually works
+// class Solution {
+//     public boolean canJump(int[] nums) {
+//         int maxLen = nums.length;
+//         boolean[] ans = new boolean[maxLen];
+//         for (int i = 0; i < maxLen; i++){
+//             ans[i] = false;
+//         }
+//         ans[maxLen-1] = true;
+//         for (int j = maxLen-2; 0 <= j; j--){
+//             if ((j+1) <= (j+nums[j])){
+//                 ans[j] = true;
+//             }
+//         }
+//         return ans[0];
+//     }
+// }
