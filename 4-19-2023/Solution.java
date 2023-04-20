054f96211ffe3780e61a581767526f0864982b24
juntab1 <jsbwhaleguy@gmail.com>
2023-04-19 21:35:20 -0700

    another solution to problem

--- 4-19-2023/Solution.java
+++ 4-19-2023/Solution.java
@@ -12,3 +12,7 @@ class Solution {
 // Better solution
 // String[] arr = s.split(" ");
 // return arr[arr.length-1].length();
+
+// Another way I could have done this is by using break and also using a for loop
+// this way might have been better. It seems like looking at the solutions this is a 
+// better solution.
